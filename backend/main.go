f3354830a2654496d4c8b20c0ecd6096a76a580c
TOFU <4416kidr@gmail.com>
2023-12-31 07:07:13 +0900

    update main.go

--- backend/main.go
+++ backend/main.go
@@ -4,6 +4,31 @@ import (
 	"fmt"
 )
 
+type Store struct {
+	Name       string
+	Location   string
+	StoreStock int
+	Comment    string
+	ImagePath  string
+}
+
+func gen_store_date(count int) []Store {
+	stores := make([]Store, count)
+
+	for i := 0; i < count; i++ {
+		store := Store{
+			Name:       "Store" + fmt.Sprint(i+1),
+			Location:   "Location of store" + fmt.Sprint(i+1),
+			StoreStock: 1,
+			Comment:    "Comment about store" + fmt.Sprint(i+1),
+			ImagePath:  "/path/to/store" + fmt.Sprint(i+1) + "/image",
+		}
+		stores[i] = store
+	}
+	return stores
+}
+
 func main() {
-	fmt.Println("hello world")
+	store_data := gen_store_date(20)
+	fmt.Println(store_data)
 }
